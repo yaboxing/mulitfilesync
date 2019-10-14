a010f4915070b48297801a6b4711203187276c98
zhexieren <zhe_xieren@163.com>
2019-10-14 15:04:36 +0800

    chore: 多线程传输文件
    work 表示 线程数量
    发送和接收的脚本配置文件可以通过 configfile 指定为同一个
    所有传输记录保存在 record 目录下, 文件名为 $usr@$ip.log
    如果需要文件记录, 则需要手动备份

--- copyfileto.sh
+++ copyfileto.sh
@@ -1,32 +1,64 @@
 #!/bin/bash
 
+start_time=`date +%s`
+
+configfile=copyfileto.cfg
+work=2
+log=record
+
 # 判断文件是否存在
-if [ ! -f copyfileto.cfg ]; then
-	echo "[copyfileto.cfg] not exist ."
+if [ ! -f $configfile ]; then
+	echo "[$configfile] not exist ."
 	# echo "usr ip pw src_dir dst_dir" > configs
 else
-	echo "[copyfileto.cfg] has created ..."
+	echo "[$configfile] has created ..."
 fi
 
-#分发到各个节点,这里分发到host文件中的主机中.
+# 增加记录文件
+if [ ! -d $log ]; then
+	mkdir $log
+	echo "[$log] created ..."
+fi
+
+# 建立有名管道用于多进程数量控制
+[ -e fd1 ] || mkfifo fd1
+exec 3<>fd1
+rm -f fd1
+
+# 建立 work 个线程
+for((i=1; i<=$work; i++)) do
+	echo >&3
+done
+
+# 分发到各个节点,这里分发到 configfile 文件中的主机中.
 while read line
 do
+	read -u3
 	usr=`echo $line | cut -d " " -f 2`
 	ip=`echo $line | cut -d " " -f 1`
 	pw=`echo $line | cut -d " " -f 3`
 	src_dir=`echo $line | cut -d " " -f 4`
 	dst_dir=`echo $line | cut -d " " -f 5`
-
 	echo "send [$src_dir] to [$usr@$ip:$dst_dir]"
+	{
+		expect <<EOF
+		set timeout 60
+		spawn rsync -av $src_dir $usr@$ip:$dst_dir
+		expect {
+			"yes/no" { send "yes\n";exp_continue }
+			"password" { send "$pw\n" }
+		}
+		expect "password" { send "$pw\n" }
 
-	expect <<EOF
-	set timeout 60
-	spawn rsync -av $src_dir $usr@$ip:$dst_dir
-	expect {
-		"yes/no" { send "yes\n";exp_continue }
-		"password" { send "$pw\n" }
-	}
-	expect "password" { send "$pw\n" }
-	# expect eof
 EOF
-done <  copyfileto.cfg
+	echo >&3
+	echo "send [$src_dir] to [$usr@$ip:$dst_dir] done."
+	} > $log/$usr@$ip.log &
+done < $configfile
+
+wait
+
+stop_time=`date +%s`
+echo "TIME:`expr $stop_time - $start_time`"
+exec 3<&-
+exec 3>&-
